168a42b356996f595d685755b7de5bf72b3a2779
Yuriev Yuriy <yuriev1986@gmail.com>
2020-09-13 16:21:45 +0300

    fix function createGallery

--- js/index.js
+++ js/index.js
@@ -26,54 +26,57 @@ const createGallery = (preview, original, description) => {
   imgRef.dataset.source = original;
   imgRef.alt = description;
 
-  // Шаг 2 - Реализация делегирования на галерее ul.js - gallery и получение url большого изображения.
-  listRef.addEventListener("click", onGalleryClick);
-  function onGalleryClick(event) {
-    event.preventDefault();
-    if (event.target.nodeName !== "IMG") {
-      return;
-    }
-    const linkRef = document.querySelector("ul > li > a");
-    const imgRef = event.target;
-    linkRef.src = imgRef.dataset.source;
-  }
-
-  // Шаг 3 открытие и закрытие модального окна
-
-  imgRef.addEventListener("click", onOpenModal);
-  closeModalBtn.addEventListener("click", onCloseModal);
-  divModalGrey.addEventListener("click", onGreyClick);
-
-  function onOpenModal() {
-    openDivModal.classList.add("is-open");
-    // Шаг 4 Подмена значения атрибута src элемента img.lightbox__image
-    modalImgRef.src = imgRef.dataset.source;
-    modalImgRef.alt = imgRef.alt;
-    window.addEventListener("keydown", onPressEsc);
-  }
-  function onCloseModal() {
-    // Шаг 5 Закрытие модального окна по клику на кнопку button[data - action= "close-modal"].
-    openDivModal.classList.remove("is-open");
-    // Шаг 6 Очистка значения атрибута src элемента img.lightbox__image
-    modalImgRef.setAttribute("src", "");
-    window.removeEventListener("keydown", onPressEsc);
-  }
-  // Дополнительно - закрытие по серому полю на модалке
-  function onGreyClick() {
-    if (event.target === event.currentTarget) {
-      onCloseModal();
-    }
-  }
-  // Дополнительно - закрытие при нажатии Esc
-  function onPressEsc(event) {
-    if (event.code === "Escape") {
-      onCloseModal();
-      console.log("Close");
-    }
-  }
-
   return liRef;
 };
+
+// Шаг 2 - Реализация делегирования на галерее ul.js - gallery и получение url большого изображения.
+listRef.addEventListener("click", onGalleryClick);
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const linkRef = document.querySelector("ul > li > a");
+  const imgRef = event.target;
+  linkRef.src = imgRef.dataset.source;
+}
+
+// Шаг 3 открытие и закрытие модального окна
+
+imgRef.addEventListener("click", onOpenModal);
+closeModalBtn.addEventListener("click", onCloseModal);
+divModalGrey.addEventListener("click", onGreyClick);
+
+function onOpenModal() {
+  openDivModal.classList.add("is-open");
+  // Шаг 4 Подмена значения атрибута src элемента img.lightbox__image
+  modalImgRef.src = imgRef.dataset.source;
+  modalImgRef.alt = imgRef.alt;
+  window.addEventListener("keydown", onPressEsc);
+}
+function onCloseModal() {
+  // Шаг 5 Закрытие модального окна по клику на кнопку button[data - action= "close-modal"].
+  openDivModal.classList.remove("is-open");
+  // Шаг 6 Очистка значения атрибута src элемента img.lightbox__image
+  modalImgRef.setAttribute("src", "");
+  window.removeEventListener("keydown", onPressEsc);
+}
+// Дополнительно - закрытие по серому полю на модалке
+function onGreyClick() {
+  if (event.target === event.currentTarget) {
+    onCloseModal();
+  }
+}
+// Дополнительно - закрытие при нажатии Esc
+function onPressEsc(event) {
+  if (event.code === "Escape") {
+    onCloseModal();
+    console.log("Close");
+  }
+}
+
+//   return liRef;
+// };
 
 pictures.forEach((picture) => {
   const liElem = createGallery(
